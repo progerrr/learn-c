049445ff8d14b498ff7e0d7fa6a2dee51ee0faa2
progerrr <bogdannovikov116@mail.ru>
2021-10-02 14:21:18 +0000

    lab4

---
+++ lab4/algo.cpp
@@ -0,0 +1,55 @@
+#include "algo.h"
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+double f(double x, int k) { return x - k * cos(x); }
+
+void fpd(double eps, int k) {
+    double x, a, b;
+    int i;
+
+    i = 0;
+    cout << "Input line [a b]:" << endl;
+    cin >> a >> b;
+
+    if (f(a, k) * f(b, k) > 0)
+        cout << "Error" << endl;
+    else {
+        while (fabs(a - b) > eps) {
+            x = (a + b) / 2;
+
+            if (f(x, k) * f(a, k) <= 0)
+                b = x;
+            else
+                a = x;
+
+            i += 1;
+        }
+
+        cout << setprecision(8) << "x= " << x << endl;
+        cout << "i = " << i << endl;
+    }
+}
+
+double df(double x, int k) { return 1 + k * sin(x); }
+
+void fn(double eps, int k) {
+    double x0;
+    int i = 0;
+
+    cout << "Input a: ";
+    cin >> x0;
+
+    double x1 = x0 - f(x0, k) / df(x0, k);
+    while (fabs(x1 - x0) > eps) {
+        x0 = x1;
+        x1 = x0 - f(x0, k) / df(x0, k);
+        i += 1;
+    }
+
+    cout << setprecision(8) << "x= " << x1 << endl;
+    cout << "i= " << i << endl;
+}
---
+++ lab4/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "algo.h"
+
+int main() {
+    double eps;
+    eps = 0.000001;
+    fpd(eps, 1);
+    fpd(eps / 100, 1);
+    fpd(eps, 10);
+
+    fn(eps, 1);
+    fn(eps / 100, 1);
+    fn(eps, 10);
+
+    return 0;
+}
