0a7367c85b4dd2a488b062d35fc0c451ca35a524
progerrr <bogdannovikov116@mail.ru>
2021-10-04 01:09:33 +0000

    lab5

---
+++ lab5/algo.cpp
@@ -0,0 +1,27 @@
+#include "algo.h"
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+void choice_array() {
+    const int length = 5;
+    int array[length] = {30, 50, 20, 10, 40};
+
+    for (int i0 = 0; i0 < length - 1; ++i0) {
+        int minI = i0;
+
+        for (int i = i0 + 1; i < length; ++i) {
+            if (array[i] < array[minI])
+
+                minI = i;
+        }
+
+        swap(array[i0], array[minI]);
+    }
+
+    for (int i = 0; i < length; ++i)
+        cout << array[i] << ' ';
+}
---
+++ lab5/main.cpp
@@ -0,0 +1,8 @@
+#include "algo.h"
+#include <iostream>
+
+int main() {
+    choice_array();
+
+    return 0;
+}
